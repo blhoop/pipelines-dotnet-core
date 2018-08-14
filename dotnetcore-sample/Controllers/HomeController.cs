cae4b3b5a4e73ded9d694aee8a5465c4a6b8d447
blhoop <blhoop@live.com>
2018-08-14 10:15:11 -0500

    Undone last changed. Build didn't fail

--- dotnetcore-sample/Controllers/HomeController.cs
+++ dotnetcore-sample/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace dotnetcore_sample.Controllers
         {
             ViewData["Message"] = "Your application description page.";
 
-            return View();;
+            return View();
         }
 
         public IActionResult Contact()
